1a2c143a72e4670c4c8ee55e8aad580908771305
judenfors <111962639+judenfors@users.noreply.github.com>
2023-10-05 11:32:01 +0200

    Update rectangle.cs

--- rectangle.cs
+++ rectangle.cs
@@ -4,6 +4,15 @@ namespace Arvuppgift
 {
     public class Rectangle: IShape
     {
-        
+        private int w;
+        private int h;
+
+        public int Area(){
+            
+        }
+
+        public int Circumference(){
+
+        }
     }
 }
